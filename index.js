c9024e79a094fb3cbbb6cb0873afc9d1072d8c38
xelita <admin@xelita.com>
2015-12-14 11:58:15 +0100

    token verification

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var uuid = require('node-uuid');
 var nJwt = require('njwt');
 
+// Signing key
 var signingKey = uuid.v4();
 console.log('signingKey: ' + signingKey);
 
+// Claims
 var claims = {
   sub: '123456789',
   iss: 'JWTGEN',
   permissions: 'lrm-access'
 }
 
-var jwt = nJwt.create(claims, signingKey)
-console.log('JWT: ' + jwt)
+// Token generation
+var token = nJwt.create(claims, signingKey).compact();
+console.log('JWT: ' + token)
+
+// Token verification
+nJwt.verify(token, signingKey, function(err, verifiedJwt) {
+  if(err){
+    console.error(err);
+  }else{
+    console.log(verifiedJwt);
+  }
+});
